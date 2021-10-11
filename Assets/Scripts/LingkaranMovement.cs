5aa7f1f7ac6f11ea2ebe39d05f98effc0e2ef895
elisiadwirahayu <81939432+elisiadwirahayu@users.noreply.github.com>
2021-10-12 00:04:58 +0700

    Problem Empat

---
+++ Assets/Scripts/LingkaranMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LingkaranMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    float horizontal;
+    float vertical;
+
+    public float maxMoveSpeed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gerakan player menggunakan WASD ataupun arrowkey
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * maxMoveSpeed, vertical * maxMoveSpeed);
+    }
+}
