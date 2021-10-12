639b80584814c9eb3d1fdf958a3027b53576dfd0
elisiadwirahayu <81939432+elisiadwirahayu@users.noreply.github.com>
2021-10-12 23:18:49 +0700

    Problem Enam

---
+++ Assets/Scripts/KotakObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KotakObject : MonoBehaviour
+{
+    public GameObject Kotak;
+    public float xMin, xMax, yMin, yMax;
+    
+    void Start()
+    {
+        // memanggil methode Spawn()
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        int penghitungObjek = Random.Range(5, 10);
+        for (int i = 0; i < penghitungObjek; i++)
+        {
+            float randomX = Random.Range(xMin, xMax);
+            float randomY = Random.Range(yMin, yMax);
+            Instantiate(Kotak, new Vector2(randomX, randomY), Quaternion.identity);
+        }
+    }
+}
