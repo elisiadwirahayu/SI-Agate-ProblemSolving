9f83605b0e7595ddf39393c108dd2f294dd8c978
elisiadwirahayu <81939432+elisiadwirahayu@users.noreply.github.com>
2021-10-13 00:24:42 +0700

    Problem Delapan

--- Assets/Scripts/KotakObject.cs
+++ Assets/Scripts/KotakObject.cs
@@ -6,11 +6,13 @@ public class KotakObject : MonoBehaviour
 {
     public GameObject Kotak;
     public float xMin, xMax, yMin, yMax;
-    
+    public static float prevx, prevy = 0;
+
+    // Start is called before the first frame update
     void Start()
     {
-        // memanggil methode Spawn()
         Spawn();
+        StartCoroutine(SpawnKotak());
     }
 
     void Spawn()
@@ -20,7 +22,48 @@ public class KotakObject : MonoBehaviour
         {
             float randomX = Random.Range(xMin, xMax);
             float randomY = Random.Range(yMin, yMax);
+
+            if (prevx < 0 && randomX < 0)
+            {
+                randomX = Mathf.Abs(randomX);
+                prevx = 0 - randomX;
+                prevy = randomY;
+            }
+            if (prevy < 0 && randomY < 0)
+            {
+                randomY = Mathf.Abs(randomY);
+                prevx = randomX;
+                prevy = 0 - randomY;
+            }
+            if (prevy > 0 && randomY > 0)
+            {
+                randomY = 0 - randomY;
+                prevx = randomX;
+                prevy = Mathf.Abs(randomY);
+            }
+            if (prevx > 0 && randomX > 0)
+            {
+                randomX = 0 - randomX;
+                prevx = Mathf.Abs(randomX);
+                prevy = randomY;
+            }
             Instantiate(Kotak, new Vector2(randomX, randomY), Quaternion.identity);
+        }
+    }
+
+    IEnumerator SpawnKotak()
+    {
+        while (true)
+        {
+            if (transform.childCount < 10)
+            {
+                float randomX = Random.Range(xMin, xMax);
+                float randomY = Random.Range(yMin, yMax);
+
+                Instantiate(Kotak, new Vector2(randomX, randomY), Quaternion.identity);
+
+            }
+            yield return new WaitForSeconds(3);
         }
     }
 }
